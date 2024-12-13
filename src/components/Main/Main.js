aa32645bb7c62449d03de611d84789d0b1c02b95
Nic2826 <nicolalgarin2605@gmail.com>
2024-12-13 00:18:38 -0500

    test

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,6 @@ import SearchForm from '../SearchForm/SearchForm'
 // import Login from '../Login/Login'
 // import Register from '../Register/Register'
 
-
 export default function Main() {
   return (
 
